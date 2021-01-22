bd229fd4d5f85da601364c243cfdb44d52c17a1f
kim noel <kimnoel@kimui-Macmini.local>
2021-01-22 11:01:25 +0900

    vuetify layout setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from "vuetify";
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
-
+Vue.use(Vuetify)
 new Vue({
+  vuetify: new Vuetify({
+    theme: {
+        themes: {
+            light: {
+            },
+        }
+    },
+    icon: {
+        iconfont: 'mdiSvg',
+    }
+}),
   render: h => h(App),
 }).$mount('#app')
